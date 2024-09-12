ab72d2c6ec6d039f1a40c6036479f39a56cb08d8
Haris Osmanagic <haris@meroxa.io>
2024-09-12 10:03:40 +0200

    updates

--- postgresql/init.sql
+++ postgresql/init.sql
@@ -4,22 +4,22 @@ CREATE TABLE employees (
     id int NOT NULL,
     name varchar(255),
     full_time bool,
-    updated_at timestamptz,
+    -- updated_at timestamptz,
     PRIMARY KEY (id)
 );
 CREATE SEQUENCE employees_id_seq;
 ALTER TABLE employees ALTER id SET DEFAULT NEXTVAL('employees_id_seq');
 ALTER TABLE employees REPLICA IDENTITY FULL;
 
-DROP TABLE IF EXISTS contractors;
-DROP SEQUENCE IF EXISTS contractors_id_seq;
-CREATE TABLE contractors (
-    id int NOT NULL,
-    name varchar(255),
-    full_time bool,
-    updated_at timestamptz,
-    PRIMARY KEY (id)
-);
-CREATE SEQUENCE contractors_id_seq;
-ALTER TABLE contractors ALTER id SET DEFAULT NEXTVAL('contractors_id_seq');
-ALTER TABLE contractors REPLICA IDENTITY FULL;
+-- DROP TABLE IF EXISTS contractors;
+-- DROP SEQUENCE IF EXISTS contractors_id_seq;
+-- CREATE TABLE contractors (
+--     id int NOT NULL,
+--     name varchar(255),
+--     full_time bool,
+--     updated_at timestamptz,
+--     PRIMARY KEY (id)
+-- );
+-- CREATE SEQUENCE contractors_id_seq;
+-- ALTER TABLE contractors ALTER id SET DEFAULT NEXTVAL('contractors_id_seq');
+-- ALTER TABLE contractors REPLICA IDENTITY FULL;
